d03a0ef09feba4beeb681963adca5b0f519ec001
nilberm <nilber16@outlook.com>
2023-01-11 21:22:34 -0300

    Prep - Week 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        
+        int magicNumberInt = randomGenerator.Next(1,101);
+
+        Console.WriteLine("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        
+        int userGuessInt = Int32.Parse(userGuess);
+
+        while (userGuessInt != magicNumberInt)
+        {
+            if (userGuessInt > magicNumberInt) 
+            {
+                Console.WriteLine("Lower");               
+                Console.WriteLine("What is your guess? ");
+
+                userGuess = Console.ReadLine();
+
+                userGuessInt = Int32.Parse(userGuess);
+            } else if (userGuessInt < magicNumberInt)
+            { 
+                Console.WriteLine("Higher");               
+                Console.WriteLine("What is your guess? ");
+
+                userGuess = Console.ReadLine();
+
+                userGuessInt = Int32.Parse(userGuess);
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+
+        List<int> arrNumber = new List<int>();
+
+        string userNumber = Console.ReadLine();
+
+        int userNumberInt = Int32.Parse(userNumber);
+
+        while (userNumberInt != 0)
+        {
+            arrNumber.Add(userNumberInt);
+            
+            Console.Write("Enter number: ");
+            userNumber = Console.ReadLine();
+            userNumberInt = Int32.Parse(userNumber);
+        }
+
+        int sum = 0;
+        int largestNum = -1;
+
+        foreach(var number in arrNumber){
+            sum += number;
+
+            if (number > largestNum){
+                largestNum = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {(float)sum / arrNumber.Count}");
+        Console.WriteLine($"The largest number is {largestNum}");
+
+
+
+        
     }
 }
