0429b90da3783a7ab874a13834bc46a772e44d0c
nilberm <nilber16@outlook.com>
2023-02-11 17:04:00 -0300

    feat: Scripture Memorizer

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,9 +1,9 @@
 public class Reference
 {
-  string _book;
-  int _chapter;
-  int _verse;
-  int _endVerse;
+  private string _book;
+  private int _chapter;
+  private int _verse;
+  private int _endVerse;
 
   public Reference()
   {
