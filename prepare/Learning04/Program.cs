9520d278d6700b7532c2a9f73cd8016551918cba
nilberm <nilber16@outlook.com>
2023-02-15 22:24:57 -0300

    feat: learning activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
-    }
+  static void Main(string[] args)
+  {
+    Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+
+    Console.WriteLine($"{assignment.GetSummary()}");
+
+    MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-9");
+
+    Console.WriteLine($"{mathAssignment.GetHomeworkList()}");
+
+    WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+
+    Console.WriteLine($"{writingAssignment.GetWritingInformation()}");
+  }
 }
