0bd74857c6ecc49c99fdb2bdab86f8e0fbd16ec9
nilberm <nilber16@outlook.com>
2023-03-01 19:38:01 -0300

    feat: polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<Shape> shapes = new List<Shape>();
+
+    Square square = new Square("Red", 4);
+    shapes.Add(square);
+
+    Rectangle rectangle = new Rectangle("Blue", 3, 7);
+    shapes.Add(rectangle);
+
+    Circle circle = new Circle("Green", 8);
+    shapes.Add(circle);
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello Learning05 World!");
+      string color = shape.GetColor();
+
+      double area = shape.GetArea();
+
+      Console.WriteLine($"The {color} shape has an area of {area}.");
     }
+  }
 }
