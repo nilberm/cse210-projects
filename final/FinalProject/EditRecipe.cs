8031f24322251d3e4a13fe339229adeced207dc5
Nilber Mota <nilber16@outlook.com>
2023-04-01 13:23:55 -0300

    feat: final project v2

--- final/FinalProject/EditRecipe.cs
+++ final/FinalProject/EditRecipe.cs
@@ -1,20 +1,20 @@
-public class EditRecipe
-{
-  private List<List<string>> _recipe;
-  private List<List<List<string>>> _recipeIngredients;
+// public class EditRecipe
+// {
+//   private List<List<string>> _recipe;
+//   private List<List<List<string>>> _recipeIngredients;
 
 
-  public void EditRecipeChosen(List<List<string>> ListRecipes, List<List<List<string>>> ListIngredients)
-  {
+//   public void EditRecipeChosen(List<List<string>> ListRecipes, List<List<List<string>>> ListIngredients)
+//   {
 
-  }
-  public List<List<string>> EditRecipe1()
-  {
-    return _recipe;
-  }
+//   }
+//   public List<List<string>> EditRecipe1()
+//   {
+//     return _recipe;
+//   }
 
-  public List<List<List<string>>> EditRecipeIngredients()
-  {
-    return _recipeIngredients;
-  }
-}
+//   public List<List<List<string>>> EditRecipeIngredients()
+//   {
+//     return _recipeIngredients;
+//   }
+// }
